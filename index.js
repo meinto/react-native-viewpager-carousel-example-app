d5efe1743b9386d4da8ce343910968178468979c
Tobias Meinhardt <tobias.meinhardt@hornbach.com>
2018-07-27 17:19:51 +0200

    Moved code from android.index.js to index.js

--- index.js
+++ index.js
@@ -3,5 +3,16 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Navigation } from 'react-native-navigation';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.RootScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.RootScreen'
+      }
+    }
+  });
+});
