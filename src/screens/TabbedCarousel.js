96b3c726a04f77dce4847c6f5709a63d427ab386
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2018-07-27 18:22:07 +0200

    Copy example from main project

--- src/screens/TabbedCarousel.js
+++ src/screens/TabbedCarousel.js
@@ -1,10 +1,72 @@
 import React, { PureComponent } from 'react'
-import { Text } from 'react-native'
+import { Text, View, StyleSheet, TouchableHighlight } from 'react-native'
+import ExamplePage from './ExamplePage'
+import { TabbedPager } from 'react-native-viewpager-carousel'
 
 export default class ComponentList extends PureComponent {
+  
+  constructor(props) {
+    super(props)
+
+    this.dataSource = []
+
+    for (let i = 1; i < 12; i++) {
+      this.dataSource = [...this.dataSource, {
+        index: i,
+        title: 'Page ' + i,
+      }]
+    }
+  }
+
+  _renderTab = ({data, _pageIndex}) => (
+    <TouchableHighlight
+      key={'tb' + data.index}
+      underlayColor={'#ccc'}
+      onPress={() => {
+        this.tabbarPager.scrollToIndex(_pageIndex)
+      }}
+    >
+      <Text style={styles.text}>{data.title}</Text>
+    </TouchableHighlight>
+  )
+
+  _renderPage = ({data}) => {
+    return (
+      <ExamplePage
+        index={data.index}
+        title={data.title}
+      />
+    )
+  }
+
   render() {
     return (
-      <Text>text</Text>
+      <View style={styles.container}>
+        <TabbedPager
+          ref={tabbarPager => {
+            this.tabbarPager = tabbarPager
+          }}
+          experimentalMirroring={false}
+          data={this.dataSource}
+          thresholdPages={2}
+          renderTab={this._renderTab}
+          renderPage={this._renderPage}
+          lazyrender={true}
+          lazyrenderThreshold={2}
+        />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  text: {
+    textAlign: 'center',
+    margin: 10,
+    padding: 10,
+  },
+})
